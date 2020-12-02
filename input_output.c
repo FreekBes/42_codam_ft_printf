9e1765ffd44bc26ee0da935ad5e83e5792042e66
Freek Bes <fbes@student.codam.nl>
2020-12-02 20:17:24 +0100

    split up handle_conv function

--- input_output.c
+++ input_output.c
@@ -6,13 +6,13 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/25 20:54:27 by fbes          #+#    #+#                 */
-/*   Updated: 2020/12/02 20:10:48 by fbes          ########   odam.nl         */
+/*   Updated: 2020/12/02 20:16:32 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	write_empty(int length)
+static void	write_empty(int length)
 {
 	if (length > 0)
 	{
@@ -21,49 +21,54 @@ void	write_empty(int length)
 	}
 }
 
-void	handle_conv(t_conv *conv, void *input)
+static int	get_written_len(t_conv *conv, void *input)
 {
-	int		written_len;
-
 	if (conv->type == 's')
-		written_len = (int)ft_strlen(input);
+		return ((int)ft_strlen(input));
 	else if (conv->type == 'c' || conv->type == '%')
-		written_len = 1;
+		return (1);
 	else if (conv->type == 'd' || conv->type == 'i')
-		written_len = ft_isneg((int)input) + ft_numlen(ft_abs((int)input), 10);
+		return (ft_isneg((int)input) + ft_numlen(ft_abs((int)input), 10));
 	else if (conv->type == 'u')
-		written_len = ft_numlen((unsigned int)input, 10);
+		return (ft_numlen((unsigned int)input, 10));
 	else if (conv->type == 'X' || conv->type == 'x')
-		written_len = -ft_isneg((int)input) + ft_numlen((unsigned int)input, 16);
+		return (-ft_isneg((int)input) + ft_numlen((unsigned int)input, 16));
 	else if (conv->type == 'p')
-		written_len = ft_ptrlen((intptr_t)input);
-	else
-		written_len = 0;
-	if (written_len < conv->width)
-		write_empty(conv->width - written_len);
+		return (ft_ptrlen((intptr_t)input));
+	return (0);
+}
+
+static int	write_output(t_conv *conv, void *input, int written_len)
+{
 	if (conv->type == 's')
 		write(1, input, (conv->precision > -1 ? conv->precision : written_len));
 	else if (conv->type == 'c')
-		ft_putchar_fd((char)input, 1);
+		written_len = ft_putchar_fd((char)input, 1);
 	else if (conv->type == 'd' || conv->type == 'i')
-		ft_putnbr_fd((int)input, 1);
+		written_len = ft_putnbr_fd((int)input, 1);
 	else if (conv->type == 'u')
-		ft_putnbr_base_fd((unsigned int)input, "0123456789", 1);
+		written_len = ft_putnbr_base_fd((unsigned int)input, "0123456789", 1);
 	else if (conv->type == 'X')
-		ft_putnbr_base_fd((int)input, "0123456789ABCDEF", 1);
+		written_len = ft_putnbr_base_fd((int)input, "0123456789ABCDEF", 1);
 	else if (conv->type == 'x')
-		ft_putnbr_base_fd((int)input, "0123456789abcdef", 1);
+		written_len = ft_putnbr_base_fd((int)input, "0123456789abcdef", 1);
 	else if (conv->type == 'p')
-		ft_putptr_fd((intptr_t)input, 1);
+		written_len = ft_putptr_fd((intptr_t)input, 1);
 	else if (conv->type == '%')
-		ft_putchar_fd('%', 1);
+		written_len = ft_putchar_fd('%', 1);
 	else
-	{
-		written_len = ft_putstr_fd("[INVALID_TYPE=", 1);
-		written_len += ft_putchar_fd(conv->type, 1);
-		written_len += ft_putchar_fd(']', 1);
-	}
+		written_len = 0;
+	return (written_len);
+}
 
+void		handle_conv(t_conv *conv, void *input)
+{
+	int		written_len;
+
+	written_len = get_written_len(conv, input);
+	if (written_len < conv->width)
+		write_empty(conv->width - written_len);
+	written_len = write_output(conv, input, written_len);
 	if (conv->width < 0 && -written_len > conv->width)
 		write_empty(-(conv->width) - written_len);
 }
