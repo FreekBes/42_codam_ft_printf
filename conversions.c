58fd61439b5368846059559988caed569a67ae0e
Freek Bes <fbes@student.codam.nl>
2020-12-09 19:50:09 +0100

    improved comments

--- conversions.c
+++ conversions.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/25 18:38:35 by fbes          #+#    #+#                 */
-/*   Updated: 2020/12/09 19:43:49 by fbes          ########   odam.nl         */
+/*   Updated: 2020/12/09 19:49:35 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,13 +74,13 @@ static int		parse_conv_simple(t_list **convs, const char **s)
 /*
 ** parse_conv_complex parses a format string in a more complex manner.
 ** it will first loop through the format string until it finds a character
-** NOT in VALID_FIELDS, which will then be set as the conversion type.
+** NOT in VALID_FIELDS and not a digit, which will then be set as the conversion type.
 ** a character pointer c is set for field flag checks. first it will check
-** if the 0 flag is present. if so, the empty character for this conversion will
-** be set to 0 (instead of the default space). then, it will check if a field width
-** if a precision has been given, by chcking if *c == '.'. if so, the precision
+** for standard field flags 0 and -, and act accordingly. then, it will check
+** for a field width, by checking if c is a digit. if so, the field width
 ** will be set to that number and the pointer c will get increased by the amount
-** of digits in that precision field, in order to check for a
+** of digits in that width field, in order to check for a precision flag.
+** if *c == '.', a precision flag is assumed to be present.
 */
 
 static int		parse_conv_complex(t_list **convs, const char **s)
@@ -132,6 +132,15 @@ static int		parse_conv_complex(t_list **convs, const char **s)
 	*s = type;
 	return (1);
 }
+
+/*
+** parse_convs parses all conversions in format string s.
+** there are two parsers available, a simple and a complex one.
+** the complex one is used if any field flags are present,
+** including widths and precisions.
+** all conversions are added to a linked list, and the last item
+** is filled with an empty conversion of type '\0'.
+*/
 
 t_list			*parse_convs(const char *s)
 {
